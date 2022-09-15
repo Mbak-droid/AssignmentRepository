27bdf8697fa5472d38c689db97fd8870f15b41f7
Melissa <mcbaker828@gmail.com>
2022-09-14 21:44:34 -0400

    changed buttons, changed response

--- joke.js
+++ joke.js
@@ -3,40 +3,34 @@ Author Melissa Baker Sept 5, 2022 - WYWM assignment - Javascript
 */
 
 //variables
-const jokeListUrl = "https://v2.jokeapi.dev"
-const jokeContainer = document.getElementById("joke");
-const btn = document.getElementById("btn");
 
-//const categories = ["Programming", "Misc", "Pun", "Spooky", "Christmas"];
+const GetJokeButton = document.getElementById('GetJoke'); //element button
+const JokeText = document.getElementById('joke'); //element div
+
+
+
 //when the page loads
-window.addEventListener("load",updateJokeList);
-//retrieve list of all jokes from api
-async function getJokeList(){
-    return fetch(jokeListUrl).then(response => response.json());
-}
-//add joke to drop down list?
-function updateJokeList(){
-    getJokeList().then(function(data){
-        //get joke list,type
-        for(var element in data.message){
-        //append to select list
-        let option = createOption(element);
-        jokeList.appendChild(option);
+GetJokeButton.addEventListener("click",retrieveJoke); //click then function called
 
-        }
-        
-      }
-    );
-}
+retrieveJoke();
 
-function createOption(text){
-    let option = document.createElement("option");
-    option.textContent = text;
-    return option;
+
+//retrieve  joke from api
+async function retrieveJoke() {
+    const returnJoke = await fetch('https://v2.jokeapi.dev/joke/Any?safe-mode', {
+       headers: {
+           'Accept':'application/json'
+       }
+    });
+  
+    const newjoke = await returnJoke.json(); //new object
+
+    console.log(newjoke);
+
+    JokeText.innerHTML = newjoke.joke;
+
 }
 
-if (typeof window !== 'undefined') {
-    console.log('You are on the browser')
-  } else {
-    console.log('You are on the server')
-  }
+
+
+
